502c0a6b39212fc1a9fccbdb9211e28c2a22e3a1
jisalguero <83598588+jisalguero@users.noreply.github.com>
2021-05-04 11:11:36 -0600

    Update Login.php

---
+++ Login.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+	<title>Informacion perosonal de la cuenta</title>
+</head>
+<body>
+
+	<form method="post">
+		<label>Usuario:</label>
+		<input type="text" name="usuario">
+
+		<label>Contraseña:</label>
+		<input type="text" name="usuario">
+
+		<input type="submit" name="ok" value="iniciar">
+
+	</form>
+
+</body>
+</html>
+
+<?php 
+
+
+
+
+
+
+ ?>
